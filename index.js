bcaba4c07629ec132728d7d79141d22fdd0b7187
hugo@122 <hugo@scorenco.com>
2023-02-23 16:07:42 +0100

    Ajout du choix de l'opérateur

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const leftOperandInput = document.getElementById("left-operand");
 const rightOperandInput = document.getElementById("right-operand");
 const resultElement = document.getElementById("result");
+const operationSelect = document.getElementById("operation");
 
 function calculate() {
   const leftOperand = Number(leftOperandInput.value);
   const rightOperand = Number(rightOperandInput.value);
-  const result = leftOperand + rightOperand;
+  let result;
+  const operation = operationSelect.value;
+  switch (operation) {
+    case "addition":
+      result = leftOperand + rightOperand;
+      break;
+    case "subtraction":
+      result = leftOperand - rightOperand;
+      break;
+    case "multiplication":
+      result = leftOperand * rightOperand;
+      break;
+    case "division":
+      result = leftOperand / rightOperand;
+      break;
+    default:
+      result = "Invalid operation";
+  }
   resultElement.textContent = result;
 }
 
 leftOperandInput.addEventListener("input", calculate);
 rightOperandInput.addEventListener("input", calculate);
+operationSelect.addEventListener("change", calculate);
 
 calculate();
