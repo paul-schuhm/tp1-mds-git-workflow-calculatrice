05182359ff7d4ab03b22e89dc38803e85ee84853
Mathis Brouard <mathisbrouard@gmail.com>
2023-06-29 17:27:13 +0200

    Ajoute le style sur la navbar et le footer. Remet l'espace de saisi en blanc

--- rendu-evaluationgitflow-b_mathis/src/App.js
+++ rendu-evaluationgitflow-b_mathis/src/App.js
@@ -43,31 +43,32 @@ function App() {
     }
 
     return (<div className={"from-indigo-500 via-purple-500 to-pink-500"}>
-        <header>
+        <div className={"flex flex-row justify-between bg-gray-800 p-8 text-white text-4xl"}>
             <h1>TP: Git workflow</h1>
-            <nav className="navbar">
+            <nav className="flex flex-row gap-8">
                 <a href="#">Accueil</a>
                 <a href="#">À propos</a>
             </nav>
-        </header>
+        </div>
 
         <body>
 
         <section>
-            <header>
-                <h2>Calculatrice en ligne</h2>
-                <p>Une application en ligne développée par ...</p>
-            </header>
-            <div className={"h-screen w-screen flex flex-col  justify-center bg-gradient-to-r "}>
+
+            <div className={"h-screen w-screen flex flex-col   bg-gradient-to-r "}>
+                <header className={"mx-auto mt-24 mb-32 text-4xl text-center text-white"}>
+                    <h2 className={"mb-3"}>Calculatrice en ligne</h2>
+                    <p>Une application en ligne développée par Mathis le Dev</p>
+                </header>
                 <div className={"mx-auto w-[40vw] flex flex-col gap-32"}>
                     <span className={"text-end px-8 text-5xl py-4 rounded-full bg-gray-800 text-white text-4xl"}>{result}</span>
-                <div className={"rounded-full bg-gray-800 text-white flex flex-row text-4xl "}>
+                <div className={"rounded-full bg-white text-black flex flex-row text-4xl "}>
                     <input type="number" value={leftOperand} onChange={handleLeftOperandChange} className="w-1/2 bg-transparent px-10 focus:outline-none" />
-                    <select value={operator} onChange={handleOperatorChange} className={"p-3 bg-transparent"}>
-                        <option value="+" className={" bg-gray-800"}>+</option>
-                        <option value="-" className={" bg-gray-800"}>-</option>
-                        <option value="*" className={" bg-gray-800"}>*</option>
-                        <option value="/" className={" bg-gray-800"}>/</option>
+                    <select value={operator} onChange={handleOperatorChange} className={"p-3 bg-white"}>
+                        <option value="+" className={" bg-white"}>+</option>
+                        <option value="-" className={" bg-white"}>-</option>
+                        <option value="*" className={" bg-white"}>*</option>
+                        <option value="/" className={" bg-white"}>/</option>
                     </select>
                     <input type="number" value={rightOperand} onChange={handleRightOperandChange} className={"px-10 w-1/2 bg-transparent text-end focus:outline-none"}/>
 
@@ -80,11 +81,7 @@ function App() {
 
         </body>
 
-        <aside>
-            Bare latérale
-        </aside>
-
-        <footer>
+        <footer className={"bg-gray-800 text-center py-10 text-4xl text-white"}>
             Le footer
         </footer>
     </div>)
